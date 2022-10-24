c80dcb9914fd7ac2c1b742a3e1fd74eefd8c0961
GGGG216 <71125715+GGGG216@users.noreply.github.com>
2022-10-24 21:38:27 +0800

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,75 +1,24 @@
-import sys
-import time
-from contral import Control
-from Runner import Runner
-
-class startUI():
-    def __init__(press):
-        startUI.choice = Control()
-        startUI.run = Runner(30)
-    def title(self):
-        print("""\033[1;47;31m       
-                    .-'''-.                                                                              
-                '   _    \                                                                            
-        /|       /   /` '.   \  __  __   ___   /|                   __  __   ___                _..._    
-        ||      .   |     \  ' |  |/  `.'   `. ||                  |  |/  `.'   `.            .'     '.  
-        ||      |   '      |  '|   .-.  .-.   '||                  |   .-.  .-.   '          .   .-.   . 
-        ||  __  \    \     / / |  |  |  |  |  |||  __              |  |  |  |  |  |    __    |  '   '  | 
-        ||/'__ '.`.   ` ..' /  |  |  |  |  |  |||/'__ '.           |  |  |  |  |  | .:--.'.  |  |   |  | 
-        |:/`  '. '  '-...-'`   |  |  |  |  |  ||:/`  '. '          |  |  |  |  |  |/ |   \ | |  |   |  | 
-        ||     | |             |  |  |  |  |  |||     | |          |  |  |  |  |  |`" __ | | |  |   |  | 
-        ||\    / '             |__|  |__|  |__|||\    / '          |__|  |__|  |__| .'.''| | |  |   |  | 
-        |/\'..' /                              |/\'..' /                           / /   | |_|  |   |  | 
-        '  `'-'`                               '  `'-'`                            \ \._,\ '/|  |   |  | 
-                                                                                    `--'  `" '--'   '--' 
-        \033[0m""")
+from pynput.keyboard import Key,Listener
 
 
+class Control():
+    def __init__(self):
+        self.dir_ = None
 
-    def progress_bar(self):
-
-        for i in range(1, 101):
-
-            print("\r", end="")
-
-            print("Download progress: {}%: ".format(i), "▋" * (i // 2), end="")
-
-            sys.stdout.flush()
-
-            time.sleep(0.05)
-
-    def menu(self):
-        menul = ['Start','Setting','Quit']
-      
-        print('\r')
-        print('\033[1;47;31muse direction key to control,↑ and --> means go right menu, ↓ and <-- means go left menu \033[0m')
-        pointer = 0
-        menul.insert(pointer,'*')
+    def getdir(self):
+        self.dir_ = None    
+        def on_press(key):
+            if key == Key.up:self.dir_ = 1
+            elif key == Key.down:self.dir_ = -1
+            elif key == Key.left:self.dir_ = -1
+            elif key == Key.right:self.dir_ = 1
+            elif key == Key.enter: self.dir_ = 2
         
-        print('\r',' '.join(menul),end="",flush=True)
-
-        choose = startUI().choice.getdir()
-        while choose!=2:
-            if pointer == 0 and choose == -1:
-                pointer = 2
-            elif pointer ==2 and choose == 1:
-                pointer = 0   
-            else:
-             
-                pointer+=choose
-            menul.remove('*')
-            menul.insert(pointer,'*')
-            print('\r',' '.join(menul),end="",flush=True)
-            choose = startUI().choice.getdir()
-        if pointer ==0:
-           startUI.run.Run()
-        elif pointer == 1:
-            pass         #add setting menu!!!!!!!!
-        elif pointer == 2:
-            pass         #add quit function !!!!!!!
-
-        
-# n = startUI()
-
-# p=n.menu()
-# print(p)
+            return False
+        listener = Listener(on_press=on_press) # 创建监听器
+        listener.start()    
+        listener.join()     
+        listener.stop()     
+        return self.dir_
+# c=Control()
+# print(c.getdir())
