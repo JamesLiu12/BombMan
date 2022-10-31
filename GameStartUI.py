83457100867c8854d02ede647a04b29e70605ef7
GGGG216 <71125715+GGGG216@users.noreply.github.com>
2022-10-31 17:15:00 +0800

    Update GameStartUI.py

--- GameStartUI.py
+++ GameStartUI.py
@@ -3,14 +3,17 @@ import time
 from UI import *
 # from Runner import Runner
 import os
+from colorama import Fore, Back, Style
+import math
 class startUI():
     def __init__(press):
+           
         startUI.choice = UI()
         # startUI.run = Runner(30)
     def title(self):
         print("""\033[1;47;31m       
-                    .-'''-.                                                                              
-                '   _    \                                                                            
+                    .-'''-.                                                   
+                   '   _    \                                                                
         /|       /   /` '.   \  __  __   ___   /|                   __  __   ___                _..._    
         ||      .   |     \  ' |  |/  `.'   `. ||                  |  |/  `.'   `.            .'     '.  
         ||      |   '      |  '|   .-.  .-.   '||                  |   .-.  .-.   '          .   .-.   . 
@@ -23,6 +26,7 @@ class startUI():
         '  `'-'`                               '  `'-'`                            \ \._,\ '/|  |   |  | 
                                                                                     `--'  `" '--'   '--' 
         \033[0m""")
+        print()
 
 
 
@@ -32,35 +36,49 @@ class startUI():
 
             print("\r", end="")
 
-            print("Download progress: {}%: ".format(i), "▋" * (i // 2), end="")
+            print("loading progress: {}%: ".format(i), "▋" * (i // 2), end="")
 
             sys.stdout.flush()
+            if i < 90:
+              time.sleep(0.05*math.ln(i))
+            else:time.sleep(0.05)
+    def show(self,choice,menul):
+        os.system ('cls')
+        self.title()
+        
+        for i in range(len(menul)):
+            if i != choice:
+            
+                print(Fore.RED+Back.WHITE+menul[i],end='')
+                print()
+            else:
 
-            time.sleep(0.05)
+                print(Fore.RED+Back.LIGHTGREEN_EX+menul[i], end = '')
+                print()
+        print(Style.RESET_ALL)
+
+        
 
     def menu(self):
         menul = ['Start','Setting','Quit']
-      
         print('\r')
         print('\033[1;47;31muse direction key to control,w and d means go right menu, s and a means go left menu,use e to choose \033[0m')
         pointer = 0
-        menul.insert(pointer,'*')
-        
-        print('\r',' '.join(menul),end="",flush=True)
-
-        choose = startUI().choice.getdir()
-        while choose!=2:
-            if pointer == 0 and choose == -1:
-                pointer = 2
-            elif pointer ==2 and choose == 1:
-                pointer = 0   
+        self.show(pointer,menul)
+        choose = self.choice.getdir()
+        while choose!=3:  #choose 
+            if pointer+choose<0:
+                pointer = len(menul)-1
+            elif pointer+choose>len(menul)-1:
+                pointer = 0  
             else:
              
                 pointer+=choose
-            menul.remove('*')
-            menul.insert(pointer,'*')
-            print('\r',' '.join(menul),end="",flush=True)
-            choose = startUI().choice.getdir()
+             
+              
+            self.show(pointer,menul)
+            
+            choose = self.choice.getdir()
         if pointer ==0:
         #    startUI.run.Run()
             pass
@@ -71,6 +89,6 @@ class startUI():
 
         
 n = startUI()
-
-p=n.menu()
-# print(p)
+# n.progress_bar()
+n.menu()
+# print(p)                    
