12918e1e77fb5346489dd7b5b42c77af2913f207
GGGG216 <71125715+GGGG216@users.noreply.github.com>
2022-11-03 11:59:38 +0800

    传入计分板

--- GameEndUI.py
+++ GameEndUI.py
@@ -1,34 +1,51 @@
-from SelectUI import *
-class GameEndUI():
+from SelectUI import SelectUI
+from numpy import *
+from colorama import Fore, Back, Style
+class GameEndUI:
     def __init__(self) -> None:
-        GameEndUI.quit = SelectUI()
+        self.quit = SelectUI
     # input winner
 
-    def ShowEndPic(self,winners,surviver):
+    def ShowEndPic(self,board,playerl,survivor):
+        
         print("""\033[1;37;40m       
-          _____                            _         _       _   _                 
-         / ____|                          | |       | |     | | (_)                
-        | |     ___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_ _  ___  _ __  ___ 
-        | |    / _ \| '_ \ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \| '_ \/ __|
-        | |___| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \__ /
-        \_____\___/|_| |_|\__, |_|  \__,_|\__|\__,_|_|\__,_|\__|_|\___/|_| |_|___/
-                            __/ |                                                  
-                            |___/                                                                                                                                               
+              _____                      ______           _ 
+             / ____|                    |  ____|         | |
+            | |  __  __ _ _ __ ___   ___| |__   _ __   __| |
+            | | |_ |/ _` | '_ ` _ \ / _ \  __| | '_ \ / _` |
+            | |__| | (_| | | | | | |  __/ |____| | | | (_| |
+             \_____|\__,_|_| |_| |_|\___|______|_| |_|\__,_|
+                                                                                                                                                                                          
         \033[0m""")
-        if surviver in winners:
+        combine = [[playerl[i],board[i]] for i in range(len(playerl))]
+        combine = array(combine)
+        combine = combine[combine[:,-1].argsort()][::-1]
+        combine = [[i[0],str(i[1])]for i in combine]
+        for i in combine: 
+           print(Fore.RED+Back.WHITE+' '.join(i))
+        maxscore = max(board)
+        winner = []
+        survive = []
+        for play in combine:
+            if play[1]==maxscore:
+                winner.append(play[0])
+        for s in survivor:
+             if s == True:
+                    survive.append(playerl[survivor.index(s)])
+        print(Fore.RED+Back.WHITE+('congratulations'+ ', '.join(winner)+'win with highest score'))
+        print(Fore.RED+Back.WHITE+('congratulations'+ ', '.join(survivor)+'survives from bomb rain'))
+        print(Style.RESET_ALL)
 
-           print(surviver.GetName(),end=' ')
-           print("\033[5;31;40msurvives and wins the game, a living legend!!!'\033[0m")
-        else:
-            print(' '.join([winner.GetName() for winner in winners]),'wins the game with highest rank!!!')
-            print(surviver.GetName(),'live to the last!!!')
-    def OutRank(self,ranklist):
+
+          
+      
+    def OutGame(self,ranklist):
         #waiting for list type
-        print('use key ENTER to quit game')
-        quit = GameEndUI.quit.getdir()
-        while quit!=2:
-              print('use key ENTER to quit game')
-              quit = GameEndUI.quit.getdir()
+        print('use q to quit game')
+        quit = self.quit.getdir()
+        while True:
+        
+              quit = self.quit.getdir()
         
 # n= GameEndUI()
 # n.endpic('Player2','Player2')
