bff8dc243881dfeac5ed639a58256daf4581dcb8
GGGG216 <71125715+GGGG216@users.noreply.github.com>
2022-11-03 13:52:41 +0800

    endUI，start加可莉

--- GameEndUI.py
+++ GameEndUI.py
@@ -1,13 +1,23 @@
 from SelectUI import SelectUI
 from numpy import *
+from GameStartUI import GameStartUI
 from colorama import Fore, Back, Style
+import os
+from Runner import Runner
 class GameEndUI:
     def __init__(self) -> None:
         self.quit = SelectUI
     # input winner
 
-    def ShowEndPic(self,board,playerl,survivor):
-        
+    def ShowEndPic(self,survivors,playerl):
+        board=[]
+        playersName = []
+        survive = []
+        for player in playerl:
+            board.append(player.GetScore())
+            playersName.append(player.GetName())
+        for survivor in survivors:
+            survive.append(player.GetName())
         print("""\033[1;37;40m       
               _____                      ______           _ 
              / ____|                    |  ____|         | |
@@ -29,23 +39,56 @@ class GameEndUI:
         for play in combine:
             if play[1]==maxscore:
                 winner.append(play[0])
-        for s in survivor:
+        for s in survivors:
              if s == True:
-                    survive.append(playerl[survivor.index(s)])
+                    survive.append(playerl[survivors.index(s)])
         print(Fore.RED+Back.WHITE+('congratulations'+ ', '.join(winner)+'win with highest score'))
-        print(Fore.RED+Back.WHITE+('congratulations'+ ', '.join(survivor)+'survives from bomb rain'))
+        print(Fore.RED+Back.WHITE+('congratulations'+ ', '.join(survivors)+'survives from bomb rain'))
         print(Style.RESET_ALL)
 
 
-          
+    def ShowMainSelect(self,choice,menul):
+        os.system ('cls')
+        self.title()
+        print('\033[1;47;31muse direction key to control,w and d means go right menu, s and a means go left menu,use e to choose \033[0m')
+        print()
+        for i in range(len(menul)):
+            if i != choice:
+            
+                print(Fore.RED+Back.BLACK+menul[i],end='')
+                print()
+            else:
+
+                print(Fore.RED+Back.WHITE+menul[i], end = '')
+                print()
+        print(Style.RESET_ALL)
+     
       
-    def OutGame(self,ranklist):
-        #waiting for list type
-        print('use q to quit game')
-        quit = self.quit.getdir()
-        while True:
-        
-              quit = self.quit.getdir()
+    def OutMenu(self):
+        menul = ['Restart','Back to menu','Quit']
+        print('\r')
+        pointer = 0
+        self.ShowMainSelect(pointer,menul)
+        choose = self.choice.getdir()
+        while choose!=3:  #choose 
+            if pointer+choose<0:
+                pointer = len(menul)-1
+            elif pointer+choose>len(menul)-1:
+                pointer = 0  
+            else:
+            
+                pointer+=choose
+            self.ShowMainSelect(pointer,menul)
+            
+            choose = self.choice.getdir()
+        if pointer ==0:
+            runner = Runner(self.settingUI.FPS, *self.settingUI.GetPlayerStates(), self.settingUI.GetmapState())
+            runner.Run()
+        elif pointer == 1:
+            newmenu = GameStartUI()
+            newmenu.menu()
+        elif pointer == 2:
+            os._exit(0)
         
 # n= GameEndUI()
 # n.endpic('Player2','Player2')
